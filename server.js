796114ee2a145ac02ae5d11117bbec39efc10558
Cameron Postnikoff <CameronJP@protonmail.com>
2023-11-08 20:44:23 -0800

    add password salting to login

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ import bodyParser from 'body-parser';
 import users from './models/usersModel.js'
 import todos from './models/todosModel.js'
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const app = express();
 
-async function generateJWT(username) {
+async function generateJWT(user) {
     try {
         const token = await jwt.sign(
-            { username: username },
+            { user: user },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
@@ -30,7 +31,7 @@ function verifyToken(req, res, next) {
             if (err) {
                 return res.status(500).send('Failed to authenticate token');
             }
-            req.user = decoded.username.username;
+            req.user = decoded;
             next();
         })
     } catch (error) {
@@ -52,28 +53,29 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     // Extract username and password from the request body (assuming they are in the request body, not query parameters)
     const { username, password } = req.query;
-        if (!username || !password) {
-            return res.status(400).send('Username and password are required');
-        }
-        else {
-            try {
-                const foundUser = await users.findOne({ username });
+    if (!username || !password) {
+        return res.status(400).send('Username and password are required');
+    }
 
-                if (!foundUser) {
-                    return res.status(401).send('Invalid credentials');
-                }
-        
-                const token = await generateJWT(foundUser);
-            
-                // Store the token in the user model (if needed)
-                foundUser.accessToken = token;
-                await foundUser.save();
-            
-                res.send(token);
-            } catch (error) {
-                return res.status(500).send('Internal server error');
-            }
+    try {
+        const foundUser = await users.findOne({ username});
+
+        if (!foundUser) {
+            return res.status(401).send('Invalid credentials');
         }
+
+        const correctPassword = await bcrypt.compare(password, foundUser.password);
+
+        if (correctPassword === true) {
+            const token = await generateJWT(foundUser.username);
+            res.send(token);
+        } else {
+            return res.status(401).send('Invalid credentials');
+        }
+
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
 });
 
 
@@ -92,8 +94,6 @@ app.post('/todos', verifyToken, async (req, res) => {
         updatedAt: creationDate
     }
 
-    console.log(newTodoObject);
-    
     await todos.create(newTodoObject);
     // TODO: validate that the todo was created properly
     res.send("post todos reached")
@@ -103,7 +103,6 @@ app.get('/todos', verifyToken, async (req, res) => {
     // Return all to-do items
     // All todos are public
     // But the user must be authenticated to request them
-    console.log(req.user)
     const { filterId, filterTitle, filterTaskDescription, filterUser } = req.query;
     const query = {
         ...(filterId && { _id: filterId }),
@@ -122,17 +121,16 @@ app.put('/todos', verifyToken, async (req, res) => {
     // If the current user is the one who crated it, update it
     // Otherwise return an error
     const { todoId, title, task } = req.query;
-
     try {
-        const exisitingTodo = await todos.findById(todoId);
-        if (exisitingTodo) {
-            if (req.user == exisitingTodo.createdBy) {
+        const existingTodo = await todos.findById(todoId);
+        if (existingTodo) {
+            if (req.user.user == existingTodo.createdBy) {
                 var updateDate = new Date();
-                if (title) exisitingTodo.title = title;
-                if (task) exisitingTodo.task = task;
-                exisitingTodo.updatedAt = updateDate;
-                await exisitingTodo.save();
-                res.send(exisitingTodo);
+                if (title) existingTodo.title = title;
+                if (task) existingTodo.task = task;
+                existingTodo.updatedAt = updateDate;
+                await existingTodo.save();
+                res.send(existingTodo);
             } else {
                 res.status(401).send('Unauthorized for editing of this todo');
             }
@@ -140,7 +138,6 @@ app.put('/todos', verifyToken, async (req, res) => {
             res.status(404).send('Todo not found');
         }
     } catch (error) {
-        console.log(error);
         res.status(500).send('Internal server error');
     }
 });
@@ -148,13 +145,13 @@ app.put('/todos', verifyToken, async (req, res) => {
 // Delete a to-do item
 app.delete('/todos', verifyToken, async (req, res) => {
     // Fetch the requested todo item,
-    // If the current user is the one who crated it, delete it
+    // If the current user is the one who created it, delete it
     // Otherwise return an error
     const { todoId } = req.query;
     try {
         const todo = await todos.findById(todoId);
         if (todo) {
-            if (req.user == todo.createdBy) {
+            if (req.user.user == todo.createdBy) {
             await todos.findByIdAndDelete(todoId);
             res.send('Todo deleted');
             } else {
@@ -164,7 +161,6 @@ app.delete('/todos', verifyToken, async (req, res) => {
             res.status(404).send('Todo not found');
         }
     } catch (error) {
-        console.log(error);
         res.status(500).send('Internal server error');
     }
 });
